4ca037528a28c96bc73f7e8b95d120895b7d9da2
lemanszczyk <lemanczyk98@gmail.com>
2023-06-12 23:07:24 +0200

    make header menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { SuperHero } from './models/super-hero';
 import { SuperHeroService } from './services/super-hero.service';
 import { AuthService } from './services/auth.service';
@@ -7,7 +7,8 @@ import { User } from './models/userLogin';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'TutorKlikUI';
