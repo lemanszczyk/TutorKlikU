673e59d24ee4e1f2199533f4e22823445e37787f
lemanszczyk <lemanczyk98@gmail.com>
2023-06-19 13:35:15 +0200

    Add angular-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule,  routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -9,7 +10,8 @@ import { AuthInterceptor } from './services/auth.interceptor';
 import { MainComponent } from './pages/main/main.component';
 import { AnnouncementComponent } from './components/announcement/announcement.component';
 import { OneAnnouncementComponent } from './pages/one-announcement/one-announcement.component';
-import { ManagementUserComponent } from './pages/management-user/management-user.component'
+import { ManagementUserComponent } from './pages/management-user/management-user.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -24,7 +26,10 @@ import { ManagementUserComponent } from './pages/management-user/management-user
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
