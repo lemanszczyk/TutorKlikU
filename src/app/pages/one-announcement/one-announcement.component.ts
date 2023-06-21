35f2b5bd2f00e45fe924099c39f8330327f73159
lemanszczyk <lemanczyk98@gmail.com>
2023-06-21 13:49:06 +0200

    Add annuncement load from server

--- src/app/pages/one-announcement/one-announcement.component.ts
+++ src/app/pages/one-announcement/one-announcement.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Announcement } from 'src/app/models/announcement';
+import { AnnouncementService } from 'src/app/services/announcement.service';
 
 @Component({
   selector: 'app-one-announcement',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./one-announcement.component.css']
 })
 export class OneAnnouncementComponent {
+  id: number = 0;
+  announcement!: Announcement;
 
+  constructor(private route: ActivatedRoute, private announcementService: AnnouncementService) {}
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      console.log(params);
+      this.id = +params['id'];
+    });
+    this.getAnnouncement();
+    console.log(this.announcement);
+  }
+  getAnnouncement() {
+    this.announcementService.getAnnoucement(this.id)
+    .subscribe((result: Announcement) => (this.announcement = result));
+    console.log(this.announcement);
+  }
 }
